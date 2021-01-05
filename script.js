f5842e6bdea375b6cf7c8f30439fcf74b288620c
Paul Ohrt <paul.ohrt@internetstiftelsen.se>
2021-01-05 12:40:54 +0100

    Refactoring objOutputMessage

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("testing");
+// Generate random messages for Git commits
+
+const objOutputMessage = {
+    actionType: [
+        'Adding', 
+        'Refactoring', 
+        'Removing'
+    ],
+
+    actionAct: [
+        'variables for input', 
+        'classes for API', 
+        'factory function'
+    ],
+
+    actionModifier: [
+        'to implement grid', 
+        'for loading DB', 
+        'as a workaround', 
+        'as a last resort'
+    ]
+    
+};
+
